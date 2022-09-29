12be400784ab4046f1fb01cfc0b6988e5105107f
gustavoBarros <gustavo.dutra11@gmail.com>
2022-09-28 22:43:19 -0300

    Alterações CSS e HTML

--- web-portfolio/src/components/Skills.jsx
+++ web-portfolio/src/components/Skills.jsx
@@ -9,15 +9,11 @@ function Skills() {
       <section className="soft-skills-summary">
     <div className="main-paragraph">
     Abaixo você verá meus resultados de mapeamento de perfil realizado no dia 18/03/2022 pela empresa MindSight, como parte do processo seletivo para ser estudante da Trybe.
-    < br />
-    < br />
     Neste gráfico está representada uma relação entre meu score e o score da média de 2353 pessoas, com perfil de analista, coordenação e gerência, de todas as áreas. Os atributos foram construídos utilizando um frame de personalidade chamado Big Five.
-    < br />
-    < br />
     O Big Five é um frame de personalidade considerado universal. Entre os psicólogos organizacionais é um consenso em termos de efetividade de avaliação.
 
-    <p>Linha azul: Score médio</p>
-    <p>Linha verde: Meu score</p>
+    <p className="legend-blue">Linha azul: Score médio</p>
+    <p className="legend-green">Linha verde: Meu score</p>
     </div>
       </section>
       <section className="img-container">
