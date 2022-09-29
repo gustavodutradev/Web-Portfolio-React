a5a380494034cf68b3af20fe3d666cc50a569e40
gustavoBarros <gustavo.dutra11@gmail.com>
2022-09-28 22:54:02 -0300

    Alteração na Home

--- web-portfolio/src/components/Home.jsx
+++ web-portfolio/src/components/Home.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import fotoHome from '../data/fotoHome.png';
 
 function Home() {
-    return (
-      <section className="home-container">  
-      <section className="slogan">
-        <section>
-          <p className="presentation-text"> Hi, I'm Gustavo.</p>
-          <h1 className="presentation-title">I build value through code and soft skills.</h1>
-          <p className="presentation-description">I'm a brazilian developer who loves to code.</p>
-        </section>
-        <section className="foto-container">
-          <img
-          src={fotoHome}
-          alt="Gustavo Barros Dutra Santos"
-          className="foto-home"
-          />
-        </section>
-        </section> 
+  return (
+    <section className="home-container">  
+    <section className="slogan">
+      <section>
+        <p className="presentation-text"> Hi, I'm Gustavo</p>
+        <p className="presentation-title">I'm a brazilian student of full stack web development.</p>
+        <h1 className="presentation-description">Focused on building value through continuous learning and teamwork.</h1>
       </section>
-    )
-}
+      <section className="foto-container">
+        <img
+        src={fotoHome}
+        alt="Gustavo Barros Dutra Santos"
+        className="foto-home"
+        />
+      </section>
+      </section> 
+    </section>
+  );
+};
 
 export default Home;
