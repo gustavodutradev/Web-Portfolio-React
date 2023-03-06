d10cc6c0cab2744dd35ee9d270113d14b7af329d
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-05 21:28:40 -0300

    unbuild Home

--- web-portfolio/src/pages/Home.jsx
+++ web-portfolio/src/pages/Home.jsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import fotoHome from '../data/fotoHome.png';
-import Footer from '../components/Footer';
 
 function Home() {
   return (
-    <section className="home-container">  
-    <section className="slogan">
-      <section>
-        <p className="presentation-text"> Hi, I'm Gustavo</p>
-        <p className="presentation-title">I'm a brazilian student of full stack web development.</p>
-        <h1 className="presentation-description">Focused on building value through continuous learning and teamwork.</h1>
-      </section>
-      <section className="foto-container">
-        <img
-        src={fotoHome}
-        alt="Gustavo Barros Dutra Santos"
-        className="foto-home"
-        />
-      </section>
-      </section> 
-      <section>
-      <Footer />
-      </section>
-    </section>
+    <section className="home-container" />
   );
-};
+}
 
 export default Home;
