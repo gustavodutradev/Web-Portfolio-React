25e56f832efeb81f0b5b5fe1928966ecf52b66a8
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-05 21:27:50 -0300

    refactor: add sandwich NavBar

--- web-portfolio/src/components/NavBar.jsx
+++ web-portfolio/src/components/NavBar.jsx
@@ -1,76 +1,102 @@
-import React from 'react'
-import { Nav, NavItem, NavLink } from 'reactstrap';
+/* eslint-disable react/jsx-max-depth */
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import BigHeads from './BigHeads';
 
 function NavBar() {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
     <div>
-        <Nav
-        style={{
-          borderBottom: `1px solid #00000033`,
-          backgroundColor: `#00000033`,
-        }}
-        >
-      <NavItem>
-        <NavLink
+      <Navbar
+        color="faded"
+        light
+        style={ {
+          border: '1px solid red',
+          height: '7rem',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          backgroundColor: '#00000033',
+        } }
+      >
+        <NavbarBrand
           href="/"
-          style={{
-            color: 'black',
-            fontWeight: 'bold',
-            fontSize: '1.1rem',
-            borderRight: `1px solid #00000033`,
-            letterSpacing: '0.1rem',
-         }}
+          className="me-auto"
+          style={ {
+            display: 'flex',
+            flexDirection: 'row',
+            width: '95%',
+            height: '100px',
+            justifyContent: 'center',
+            border: '1px solid red',
+          } }
         >
-          HOME
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          href="/projects"
-          style={{
-            color: 'black',
-            fontWeight: 'bold',
-            fontSize: '1.1rem',
-            borderRight: `1px solid #00000033`,
-            letterSpacing: '0.1rem',
-         }}
-         >
-         PROJECTS
-        </NavLink>
-      </NavItem>
-
-    <NavItem>
-      <NavLink
-      href="/skills"
-      style={{
-        color: 'black',
-        fontWeight: 'bold',
-        fontSize: '1.1rem',
-        borderRight: `1px solid #00000033`,
-        letterSpacing: '0.1rem',
-     }}
-      >
-      SKILLS
-      </NavLink>
-    </NavItem>
-
-    <NavItem>
-      <NavLink
-        href="/about"
-        style={{
-          color: 'black',
-          fontWeight: 'bold',
-          fontSize: '1.1rem',
-          borderRight: `1px solid #00000033`,
-          letterSpacing: '0.1rem',
-       }}
-      >
-       ABOUT
-      </NavLink>
-    </NavItem>
-  </Nav>
+          <BigHeads />
+        </NavbarBrand>
+        <NavbarToggler onClick={ toggleNavbar } className="me-2" />
+        <Collapse isOpen={ !collapsed } navbar style={ { backgroundColor: '#00000033' } }>
+          <Nav navbar>
+            <NavItem>
+              <NavLink
+                href="/"
+                style={ {
+                  fontSize: '1.3rem',
+                  fontWeight: 'bold',
+                } }
+              >
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="/projects"
+                style={ {
+                  fontSize: '1.3rem',
+                  fontWeight: 'bold',
+                } }
+              >
+                Projects
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="/skills"
+                style={ {
+                  fontSize: '1.3rem',
+                  fontWeight: 'bold',
+                } }
+              >
+                Skills
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="/about"
+                style={ {
+                  fontSize: '1.3rem',
+                  fontWeight: 'bold',
+                } }
+              >
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
-  )
+  );
 }
 
 export default NavBar;
