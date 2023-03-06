ae8e2f3fda0b3cc595059ddcd9749db95da7470a
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-05 21:27:00 -0300

    feat: add BigHead size

--- web-portfolio/src/components/BigHeads.jsx
+++ web-portfolio/src/components/BigHeads.jsx
@@ -4,30 +4,34 @@ import { BigHead } from '@bigheads/core';
 function BigHeads() {
   return (
     <div>
-        <BigHead
-    accessory="roundGlasses"
-    body="chest"
-    circleColor="blue"
-    clothing="shirt"
-    clothingColor="black"
-    eyebrows="raised"
-    eyes="happy"
-    faceMask={false}
-    faceMaskColor="red"
-    facialHair="stubble"
-    graphic="react"
-    hair="short"
-    hairColor="black"
-    hat="none2"
-    hatColor="blue"
-    lashes
-    lipColor="red"
-    mask
-    mouth="grin"
-    skinTone="light"
-  />
+      <BigHead
+        accessory="roundGlasses"
+        body="chest"
+        circleColor="blue"
+        clothing="shirt"
+        clothingColor="black"
+        eyebrows="raised"
+        eyes="happy"
+        faceMask={ false }
+        faceMaskColor="red"
+        facialHair="stubble"
+        graphic="react"
+        hair="short"
+        hairColor="black"
+        hat="none2"
+        hatColor="blue"
+        lashes
+        lipColor="red"
+        mask
+        mouth="grin"
+        skinTone="light"
+        style={ {
+          width: '5rem',
+          height: '5rem',
+        } }
+      />
     </div>
-  )
+  );
 }
 
-export default BigHeads
+export default BigHeads;
