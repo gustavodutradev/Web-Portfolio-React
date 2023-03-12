5e10137ebd08daed6c9c1915f84c6bedbb0d56af
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-12 12:46:36 -0300

    feat: Home about-text

--- web-portfolio/src/pages/Home.jsx
+++ web-portfolio/src/pages/Home.jsx
@@ -1,8 +1,28 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 function Home() {
   return (
-    <section className="home-container" />
+    <section className="home-container">
+      <section />
+      <section className="about-container">
+        <p className="about-text">
+          Olá, bem-vindo ao meu portfólio! Meu nome é Gustavo, tenho 30 anos e estou em transição de carreira.
+          Oriundo da Engenharia Ambiental, decidi buscar novos desafios e encontrei na programação uma nova paixão.
+          Atualmente estou estudando Desenvolvimento Web Full Stack na Trybe, onde aprendo as tecnologias mais utilizadas no mercado de trabalho. Estou em busca de uma oportunidade de trabalho para colocar em prática os conhecimentos adquiridos e me tornar um profissional cada vez melhor.
+        </p>
+      </section>
+      <section className="projects-container">
+        PROJETOS
+      </section>
+      <section className="soft-skills-container">
+        HABILIDADES COMPORTAMENTAIS
+      </section>
+      <section className="contact-container">
+        CONTATO
+      </section>
+    </section>
+
   );
 }
 
