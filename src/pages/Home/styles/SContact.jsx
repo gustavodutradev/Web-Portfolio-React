c3d71adf1755b5db251cd8690beda561c64f9c11
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-17 18:03:09 -0300

    feat: uncomment SContact style

--- src/pages/Home/styles/SContact.jsx
+++ src/pages/Home/styles/SContact.jsx
@@ -3,11 +3,12 @@ import tw from 'twin.macro';
 
 const SContact = styled.section`
   ${tw`
-  // flex
-  // flex-col
-  // justify-center
-  // items-center
+  flex
+  flex-col
+  justify-center
+  items-center
   `}
+  width: 100%;
 `;
 
 export default SContact;
