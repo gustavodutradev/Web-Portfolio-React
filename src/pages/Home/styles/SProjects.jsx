10fc0e5960c3fd3e472b838aa3d34b6747b82f5c
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-17 16:39:11 -0300

    feat: Home-Projects container style file

---
+++ src/pages/Home/styles/SProjects.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const SProjects = styled.section`
+  ${tw`
+  flex
+  flex-col
+  justify-center
+  items-center
+  `}
+  padding: 20px;
+  gap: 2rem;
+`;
+
+export default SProjects;
