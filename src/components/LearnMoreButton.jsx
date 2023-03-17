b61c77c7eb606cd5284d3b2624c9cec8d181136c
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-17 16:35:43 -0300

    feat: add style to Button component

--- src/components/LearnMoreButton.jsx
+++ src/components/LearnMoreButton.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styled from 'styled-components';
 
+const SButton = styled.button`
+outline: none;
+border: none;
+background-color: #ff8c00;
+color: white;
+padding: 1rem auto;
+height: 30px;
+width: 90px;
+border-radius: 20px;
+font-size: 12px;
+font-weight: bold;
+transition: 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+&:hover {
+  scale: 1.05;
+  color: black;
+}
+&:active {
+  scale: 0.95;
+}
+`;
 function Button(props) {
   const { onClick, name } = props;
   return (
-    <button
+    <SButton
       type="button"
       onClick={ onClick }
       name={ name }
     >
       { name }
-    </button>
+    </SButton>
   );
 }
 
