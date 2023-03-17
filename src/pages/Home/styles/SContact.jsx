32957874a0e5e9cb82b2aa915b2025876d608d69
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-17 16:39:23 -0300

    feat: Home-Contact container style file

---
+++ src/pages/Home/styles/SContact.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const SContact = styled.section`
+  ${tw`
+  // flex
+  // flex-col
+  // justify-center
+  // items-center
+  `}
+`;
+
+export default SContact;
