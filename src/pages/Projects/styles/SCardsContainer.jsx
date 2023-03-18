cb28cb8613d87a8f2528158b38989b023a934f1c
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-18 16:06:12 -0300

    feat: SCard

---
+++ src/pages/Projects/styles/SCardsContainer.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const SCard = styled.section`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 2fr));
+`;
+
+export default SCard;
