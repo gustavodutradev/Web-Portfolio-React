c1a091c6bb7fa003422adb82c37ba98bb113cd7f
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-13 15:28:53 -0300

    feat: create a styled component to the repo link

---
+++ src/pages/Projects/styles/SRepoLink.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const SLink = styled.a`
+    ${tw`
+    text-white
+    text-sm
+    font-bold
+    `}
+    display: flex;
+    text-decoration: none;
+    transition: all 0.2s ease-in-out;
+    border: 1px solid orange;
+    border-radius: 20px;
+    padding: 0.2rem 0.5rem;
+    margin: 0.2rem 0.5rem;
+    width: fit-content;
+    align-items: center;
+    background-color: black;
+    transition: all 0.2s ease-in-out;
+    color: #fff;
+    &:hover {
+        color: orange;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        transform: scale(1.05);
+    }
+`;
+
+export default SLink;
