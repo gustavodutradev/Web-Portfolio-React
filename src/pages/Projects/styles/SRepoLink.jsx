f0989dedfe76d5f5ffcd7f7fc786fa77ced64df4
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-14 15:19:19 -0300

    feat: implement active event

--- src/pages/Projects/styles/SRepoLink.jsx
+++ src/pages/Projects/styles/SRepoLink.jsx
@@ -12,17 +12,20 @@ const SLink = styled.a`
     transition: all 0.2s ease-in-out;
     border: 1px solid orange;
     border-radius: 20px;
-    padding: 0.2rem 0.5rem;
+    padding: 0.4rem 0.7rem;
     margin: 0.2rem 0.5rem;
     width: fit-content;
     align-items: center;
-    background-color: black;
-    transition: all 0.2s ease-in-out;
+    background-color: #121212;
+    transition: all 0.1s ease-in-out;
     color: #fff;
     &:hover {
         color: orange;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
         transform: scale(1.05);
+    }
+    &:active {
+    scale: 0.95;
     }
 `;
 
