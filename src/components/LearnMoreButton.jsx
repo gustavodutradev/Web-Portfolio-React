6cd19640a997aeb5e4b7fa376fc5eabdd2f5bc30
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-19 15:12:07 -0300

    refactor: learn more button

--- src/components/LearnMoreButton.jsx
+++ src/components/LearnMoreButton.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
 import propTypes from 'prop-types';
+import React from 'react';
 import styled from 'styled-components';
 
 const SButton = styled.button`
 outline: none;
 border: none;
-background-color: #ff8c00;
-color: white;
+background: #f09819;
+background: -webkit-linear-gradient(0deg, #f09819 0%, #edde5d 100%);
+background: linear-gradient(0deg, #f09819 0%, #edde5d 100%);
+color: #ffffff;
+text-shadow: 2px 1px 2px #121212ad;
 padding: 1rem auto;
-height: 30px;
-width: 90px;
-border-radius: 20px;
-font-size: 12px;
+height: auto;
+width: 110px;
+border-radius: 25px;
+font-size: 0.9rem;
 font-weight: bold;
 transition: 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 &:hover {
   scale: 1.05;
-  color: black;
 }
 &:active {
   scale: 0.95;
