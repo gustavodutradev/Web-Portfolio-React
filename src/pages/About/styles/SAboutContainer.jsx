55dd76228005961b4e062034fa694fa13f2d289b
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-29 15:33:10 -0300

    feat: create SAboutContainer

---
+++ src/pages/About/styles/SAboutContainer.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const SAboutContainer = styled.section`
+    ${tw`
+
+    `}
+    height: 100vh;
+    max-width: 100vmax;
+    background: #161616;
+    background: -webkit-linear-gradient(0deg, #161616 0%, #1488cc 100%);
+    background: linear-gradient(0deg, #161616 0%, #1488cc 100%);
+`;
+
+export default SAboutContainer;
