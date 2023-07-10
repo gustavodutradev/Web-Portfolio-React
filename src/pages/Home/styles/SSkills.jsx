9cb479549a45c2a87ba969db08d3912cd6af1213
gustavoBarros <gustavo.dutra11@gmail.com>
2023-07-10 13:40:33 -0300

    feat: some refactors and implement pulse animation

--- src/pages/Home/styles/SSkills.jsx
+++ src/pages/Home/styles/SSkills.jsx
@@ -8,16 +8,37 @@ const SSkills = styled.section`
   justify-center
   items-center
   `}
+  max-width: 90vw;
+  height: auto;
   img {
     max-width: 100px;
     height: auto;
     filter: drop-shadow(3px 3px 3px #1f1f1f);
     padding: 1rem;
+    border-radius: 15px;
+
+    &:hover {
+        color: orange;
+        animation: pulse 2s infinite;
+    }
   }
-  img:hover {
-    transform: scale(1.2);
-    transition: all 0.2s ease-in-out;
-  }
+
+@keyframes pulse {
+0% {
+transform: scale(0.95);
+box-shadow: 0 0 0 0 #006494;
+}
+
+70% {
+transform: scale(1.05);
+box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+}
+
+100% {
+transform: scale(0.95);
+box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+}
+}
 
   @media (max-width: 480px) {
     img {
