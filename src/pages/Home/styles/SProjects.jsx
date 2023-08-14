b0478157eba2b1f0c5cc804ff7f4499f2f368aff
gustavoBarros <gustavo.dutra11@gmail.com>
2023-08-14 12:23:52 -0300

    deleted: old navbars

--- src/pages/Home/styles/SProjects.jsx
+++ src/pages/Home/styles/SProjects.jsx
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
 const SProjectsContainer = styled.section`
-@media (max-width: 480px) {
   display: flex;
   max-width: 90vw;
   height: auto;
   overflow-x: scroll;
   scroll-snap-type: x proximity;
-}
+
 `;
 
 export default SProjectsContainer;
