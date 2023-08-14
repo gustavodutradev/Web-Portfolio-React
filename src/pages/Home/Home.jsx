389e4a9d247cbf402a4ca3d451bbdc9c8706aa25
gustavoBarros <gustavo.dutra11@gmail.com>
2023-08-14 12:23:29 -0300

    feat: implement projects card on Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Carrousel from '../../components/Carrousel';
 import Button from '../../components/LearnMoreButton';
+import TFC from '../../data/reposImages/TFC.png';
+import login from '../../data/reposImages/loginmockup.png';
+import onlinestore from '../../data/reposImages/onlinestore1.png';
 import skillsImagesArray from '../../helpers/skillsImagesArray';
 
 import SAbout from './styles/SAbout';
 import SHomeContainer from './styles/SHomeContainer';
-import SProjects from './styles/SProjects';
+import SProjectCard from './styles/SProjectCard';
+import SProjectsContainer from './styles/SProjects';
 import SSkills from './styles/SSkills';
 
 function Home() {
   const navigate = useNavigate();
+  const repo = 'Repositório';
 
   return (
     <SHomeContainer>
@@ -38,13 +42,36 @@ function Home() {
         }
       </SSkills>
 
-      <SProjects>
-        <Carrousel />
-        <Button
-          onClick={ () => navigate('/projects') }
-          name="Veja todos os projetos"
-        />
-      </SProjects>
+      <SProjectsContainer>
+        <SProjectCard>
+          <h3> App Delivery </h3>
+          <img src={ login } alt="project" />
+          <p> MPV Aplicativo Delivery de Bebidas </p>
+          <a href="https://github.com/Gustavo-trybedev/App-Delivery" target="_blank" rel="noreferrer">
+            { repo }
+          </a>
+        </SProjectCard>
+        <SProjectCard>
+          <h3> Online Store </h3>
+          <img src={ onlinestore } alt="project" />
+          <p> Market Place baseado no Mercado Livre </p>
+          <a href="https://github.com/Gustavo-trybedev/Project-Online-Store" target="_blank" rel="noreferrer">
+            { repo }
+          </a>
+        </SProjectCard>
+        <SProjectCard>
+          <h3> Trybe Futebol Clube </h3>
+          <img src={ TFC } alt="project" />
+          <p>Tabela de classificação de times em TypeScript e POO</p>
+          <a href="https://github.com/Gustavo-trybedev/TFC" target="_blank" rel="noreferrer">
+            { repo }
+          </a>
+        </SProjectCard>
+      </SProjectsContainer>
+      <Button
+        onClick={ () => navigate('/projects') }
+        name="Veja todos os projetos"
+      />
     </SHomeContainer>
   );
 }
