505161ec8f8d0b38272fa19c3a56f6edc9fd485d
gustavoBarros <gustavo.dutra11@gmail.com>
2023-08-14 13:29:38 -0300

    feat: add screen media query

--- src/pages/Home/styles/SProjects.jsx
+++ src/pages/Home/styles/SProjects.jsx
@@ -4,9 +4,12 @@ const SProjectsContainer = styled.section`
   display: flex;
   max-width: 90vw;
   height: auto;
-  overflow-x: scroll;
+  overflow-x: hidden;
   scroll-snap-type: x proximity;
 
+  @media (max-width: 480px) {
+    overflow-x: scroll;
+  }
 `;
 
 export default SProjectsContainer;
