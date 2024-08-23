ab50bc73202a6a230910e41a0bf0b1318f2cc5a4
gustavodutradev <gustavobdsdev@gmail.com>
2024-08-23 15:37:30 -0300

    refactor: change learn more button padding

--- src/components/LearnMoreButton.jsx
+++ src/components/LearnMoreButton.jsx
@@ -10,7 +10,7 @@ background: -webkit-linear-gradient(0deg, #f09819 0%, #edde5d 100%);
 background: linear-gradient(0deg, #f09819 0%, #edde5d 100%);
 color: #ffffff;
 text-shadow: 2px 1px 2px #121212ad;
-padding: 1rem auto;
+padding: 5px;
 height: auto;
 width: 110px;
 border-radius: 25px;
@@ -22,6 +22,12 @@ transition: 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
 &:active {
   scale: 0.95;
+}
+
+@media (max-width: 480px) {
+  width: 100px;
+  font-size: 0.8rem;
+  padding: 0.4rem;
 }
 `;
 function Button(props) {
