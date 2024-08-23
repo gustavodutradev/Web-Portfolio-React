dd55ee946acd020cb4cad4356347c5c741893a01
gustavodutradev <gustavobdsdev@gmail.com>
2024-08-23 09:17:41 -0300

    Fix: change github username on URL

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,8 +1,9 @@
 const getMyReposFromGithub = async () => {
   try {
-    const URL = 'https://api.github.com/users/Gustavo-trybedev/repos';
+    const URL = 'https://api.github.com/users/gustavodutradev/repos';
     const request = await fetch(URL);
     const result = await request.json();
+    console.log(result);
     return result;
   } catch (error) {
     console.log(error, 'Failed to request repos');
