9ef0a744a22f3fc536deb355b67d6c2fb6355815
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-08-26 18:43:20 -0300

    refactor: changed height and padding at projects container to better fit

--- src/pages/Projects/styles/SProjectsContainer.jsx
+++ src/pages/Projects/styles/SProjectsContainer.jsx
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 const SProjectsContainer = styled.section`
     width: 100%;
     height: 100%;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     gap: 2rem;
-    padding: 6rem 2rem 2rem 2rem;
+    padding: 12rem 2rem 2rem 2rem;
     background: #161616;
     background: -webkit-linear-gradient(0deg, #161616 0%, #1488cc 100%);
     background: linear-gradient(0deg, #161616 0%, #1488cc 100%);
