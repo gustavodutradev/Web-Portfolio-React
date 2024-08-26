7181b1d1fa0d3b6b97ab41cb03fd6a7b17295493
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-08-26 18:41:29 -0300

    refactor: deleted TFC card and changed images alt text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/LearnMoreButton';
-import TFC from '../../data/reposImages/TFC.png';
+// import TFC from '../../data/reposImages/TFC.png';
 import login from '../../data/reposImages/loginmockup.png';
 import onlinestore from '../../data/reposImages/onlinestore1.png';
 import skillsImagesArray from '../../helpers/skillsImagesArray';
@@ -45,7 +45,7 @@ function Home() {
       <SProjectsContainer>
         <SProjectCard>
           <h3> App Delivery </h3>
-          <img src={ login } alt="project" />
+          <img src={ login } alt="Delivery App" />
           <p> MPV Aplicativo Delivery de Bebidas </p>
           <a href="https://github.com/Gustavo-trybedev/App-Delivery" target="_blank" rel="noreferrer">
             { repo }
@@ -53,20 +53,20 @@ function Home() {
         </SProjectCard>
         <SProjectCard>
           <h3> Online Store </h3>
-          <img src={ onlinestore } alt="project" />
+          <img src={ onlinestore } alt="Online Store" />
           <p> Market Place baseado no Mercado Livre </p>
           <a href="https://github.com/Gustavo-trybedev/Project-Online-Store" target="_blank" rel="noreferrer">
             { repo }
           </a>
         </SProjectCard>
-        <SProjectCard>
+        {/* <SProjectCard>
           <h3> Trybe Futebol Clube </h3>
-          <img src={ TFC } alt="project" />
+          <img src={ TFC } alt="Trybe Football Club" />
           <p>Tabela de classificação de times em TypeScript e POO</p>
           <a href="https://github.com/Gustavo-trybedev/TFC" target="_blank" rel="noreferrer">
             { repo }
           </a>
-        </SProjectCard>
+        </SProjectCard> */}
       </SProjectsContainer>
       <Button
         onClick={ () => navigate('/projects') }
